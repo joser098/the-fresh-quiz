ac5b2a9ae976cb9224d4420585925e67c8a4bfdb
josejaramillo098@gmail.com <josejaramillo098@gmail.com>
2023-09-06 12:33:25 -0300

    fix: changes in categoryCard component

--- src/components/CategoryCard/CategoryCard.jsx
+++ src/components/CategoryCard/CategoryCard.jsx
@@ -2,11 +2,13 @@ import s from "./CategoryCard.module.css";
 
 const CategoryCard = ({ id, icon, title, questions }) => {
   return (
-    <a href={`/questionary/${id}`} className={s.container}>
-      <img src={`https://quiz-7.com/${icon}`} alt={title} />
-      <h3>{title}</h3>
-      <p>{questions} Questions</p>
-    </a>
+    <>
+      <a href={`/questionary/${id}`} className={s.container}>
+        <img src={`https://quiz-7.com/${icon}`} alt={title} />
+        <h3>{title}</h3>
+        <p>{questions} Questions</p>
+      </a>
+    </>
   );
 };
 
