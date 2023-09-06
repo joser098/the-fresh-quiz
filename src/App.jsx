a1befcfb71eea9e12bde6499f9428108534d117c
josejaramillo098@gmail.com <josejaramillo098@gmail.com>
2023-09-06 12:32:22 -0300

    feat: react router dom added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
+import Questionary from "./components/Questionary/Questionary";
 
 function App() {
   return (
     <>
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="questionary/:id" element={<Questionary />}/>
+      </Routes>
     </>
   );
 }
