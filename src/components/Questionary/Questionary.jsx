97bc36acf08b0b7dfc6d1682b272ace66618622a
josejaramillo098@gmail.com <josejaramillo098@gmail.com>
2023-09-07 01:59:56 -0300

    feat: Questionary component

--- src/components/Questionary/Questionary.jsx
+++ src/components/Questionary/Questionary.jsx
@@ -1,8 +1,11 @@
 import { useParams } from "react-router-dom";
 import { useQuestionaryStore } from "../../store/questionary";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import s from "./Questionary.module.css";
+import Question from "../Question/Question";
 
 const Questionary = () => {
+  const [progressBar, setProgressBar] = useState(0);
   const { id } = useParams();
   const currentQuestionary = useQuestionaryStore(
     (state) => state.currentQuestionary
@@ -10,9 +13,25 @@ const Questionary = () => {
   const setCurrentQuestionary = useQuestionaryStore(
     (state) => state.setCurrentQuestionary
   );
+  const optionSelected = useQuestionaryStore((state) => state.optionSelected);
+  const setOptionSelected = useQuestionaryStore(
+    (state) => state.setOptionSelected
+  );
+  const setTimerExpired = useQuestionaryStore((state) => state.setTimerExpired);
+
+  const handleNextButton = () => {
+    setProgressBar(progressBar + 1);
+
+    if (optionSelected === "option1") {
+      alert("respuesta correcta");
+    } else {
+      alert("respuesta incorrecta");
+    }
+
+    setTimerExpired(false);
+  };
 
   useEffect(() => {
-    console.log(id);
     const fetchQuestionary = async (quesId) => {
       await setCurrentQuestionary(quesId);
     };
@@ -21,10 +40,26 @@ const Questionary = () => {
   }, []);
 
   return (
-    <>
-      <h4>{id}</h4>
-      <p>{currentQuestionary[0]?.question}</p>
-    </>
+    <section className={`flex ${s.container}`}>
+      <div className={`flex ${s.header}`}>
+        <a href="/">X</a>
+        <div className={s.bar}>
+          <progress
+            max={currentQuestionary.length}
+            value={progressBar}
+            className={s.progress}
+          />
+          <span>
+            {progressBar}/{currentQuestionary.length}
+          </span>
+        </div>
+      </div>
+      <Question currentQuestion={currentQuestionary[progressBar]} />
+      <button className={s.nextBtn} onClick={handleNextButton}>
+        {" "}
+        Next{" "}
+      </button>
+    </section>
   );
 };
 
