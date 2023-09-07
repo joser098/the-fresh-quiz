1547e1bc45eb47ae5c287097025709654f53c608
josejaramillo098@gmail.com <josejaramillo098@gmail.com>
2023-09-07 16:14:11 -0300

    feat:App styles changed for bg color

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import "./App.css";
-import { Route, Routes } from "react-router-dom";
+import s from "./App.module.css";
+import { Route, Routes, useLocation } from "react-router-dom";
 import Home from "./components/Home";
 import Questionary from "./components/Questionary/Questionary";
 
 function App() {
+  const location = useLocation();
+
   return (
-    <>
+    <main className={`${location.pathname === "/" ? s.home : s.questionary}`}>
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="questionary/:id" element={<Questionary />}/>
       </Routes>
-    </>
+    </main>
   );
 }
 
